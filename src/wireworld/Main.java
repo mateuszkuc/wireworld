b27bf26c71614443fa48226c8c28c5034f7c1d2c
mateuszkuc <mdg.kuc@gmail.com>
2019-05-27 18:35:38 +0200

    Dodanie klasy main

---
+++ src/wireworld/Main.java
@@ -0,0 +1,22 @@
+package wireworld;
+
+import javax.swing.JFrame;
+
+public class Main
+{
+
+    public Main()
+    {
+    }
+
+    public void start()
+    {
+    }
+
+    public static void main(String[] args)
+    {
+        Main m = new Main();
+        m.start();
+    }
+}
+
